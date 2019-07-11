0c753d15ed932e2c6d866565849eea78c263d613
TeTeBerry <250714751@qq.com>
2019-07-12 02:58:29 +0700

    Merge branch 'master' of https://github.com/TeTeBerry/Spring-iot
    # Conflicts:
    #	logs/error.log
    #	logs/log.2019-05-08.log
    #	logs/log.log
    #	src/main/resources/mapper/MeterMapper.xml
    #	src/main/resources/mapper/UserMapper.xml

--- src/main/java/com/iot/smart/water/meter/IotSmartWaterMeterApplication.java
+++ src/main/java/com/iot/smart/water/meter/IotSmartWaterMeterApplication.java
@@ -1,11 +1,5 @@
 package com.iot.smart.water.meter;
 
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@LineMessageHandler
+
 public class IotSmartWaterMeterApplication {
 
 	public static void main(String[] args) {
@@ -21,12 +15,6 @@ public class IotSmartWaterMeterApplication {
 	}
 
 
-	@EventMapping
-	public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
-		System.out.println("event: " + e);
-		TextMessageContent message = e.getMessage();
-		return new TextMessage(message.getText());
-	}
 
 }
 
