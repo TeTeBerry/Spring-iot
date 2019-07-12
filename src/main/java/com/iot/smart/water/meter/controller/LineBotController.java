e1ccd5e596577b9202927088ae01e54189f73e9d
TeTeBerry <250714751@qq.com>
2019-07-12 07:36:48 +0700

    Merge branch 'master' of https://github.com/TeTeBerry/Spring-iot
    # Conflicts:
    #	logs/error.log
    #	logs/log.2019-05-08.log
    #	logs/log.log
    #	src/main/resources/mapper/MeterMapper.xml
    #	src/main/resources/mapper/UserMapper.xml

--- src/main/java/com/iot/smart/water/meter/controller/LineBotController.java
+++ src/main/java/com/iot/smart/water/meter/controller/LineBotController.java
@@ -1,26 +1,46 @@
 package com.iot.smart.water.meter.controller;
 
-import com.linecorp.bot.model.event.Event;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
-
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.ExecutionException;
 
 
 @LineMessageHandler
 public class LineBotController {
 
-    @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
-    }
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
 
     @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent>event) throws Exception {
+        final LineMessagingClient client = LineMessagingClient
+                .builder("dRaF1mGY8pDpxRStCA7sKp+HP4CcI/t7tTH0N8S98mz1r3b7SJN3UmJHG77hsaiWMe+MTCebi3Df355ZBTPpFAliAlJN2R6hhviDlpWAmvzeLn+9VMsjkzv0su8WHUf1wMz/Aeana12Xy2binCTVzAdB04t89/1O/w1cDnyilFU=")
+                .build();
+
+        final TextMessage textMessage = new TextMessage("hello");
+        final PushMessage pushMessage = new PushMessage(
+                "world",
+                textMessage);
+
+        final BotApiResponse botApiResponse;
+        try {
+            botApiResponse = client.pushMessage(pushMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println(botApiResponse);
     }
 }
